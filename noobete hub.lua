42b450c0cf215cc75504c95fbacb55dcc2e0cae6
Noobie <130820910+NoobieEpic@users.noreply.github.com>
2025-01-26 08:17:06 -0800

    Noobete Hub Ported To Rayfield (orion broke)

--- noobete hub.lua
+++ noobete hub.lua
@@ -1,3 +1,4 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
 	Name = "NoobeteHub",
 	Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
@@ -11,7 +12,7 @@ local Window = Rayfield:CreateWindow({
 	ConfigurationSaving = {
 	   Enabled = true,
 	   FolderName = nil, -- Create a custom folder for your hub/game
-	   FileName = "Big Hub"
+	   FileName = "NoobeteHub"
 	},
  
 	Discord = {
