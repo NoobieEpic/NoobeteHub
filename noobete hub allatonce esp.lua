ff908ae6f440200581ef2b8704965b2c802216d2
Noobie <130820910+NoobieEpic@users.noreply.github.com>
2025-01-26 08:09:45 -0800

    The Noobete Hub ESP

---
+++ noobete hub allatonce esp.lua
@@ -0,0 +1,14 @@
+local BartHIghlight = Instance.new("Highlight")
+local HomerHIghlight = Instance.new("Highlight")
+local KrustyHIghlight = Instance.new("Highlight")
+local MaggieHIghlight = Instance.new("Highlight")
+local LisaHIghlight = Instance.new("Highlight")
+local MargeHIghlight = Instance.new("Highlight")
+local MonsterHIghlight = Instance.new("Highlight")
+BartHIghlight.Parent = workspace.Ai.Bart
+HomerHIghlight.Parent = workspace.Ai.Homer
+KrustyHIghlight.Parent = workspace.Ai.Krusty
+MaggieHIghlight.Parent = workspace.Ai.Lisa
+LisaHIghlight.Parent = workspace.Ai.Maggie
+MargeHIghlight.Parent = workspace.Ai.Marge
+MonsterHIghlight.Parent = workspace.Ai.Monster
